ad4eb76514f97c84292dcc061066290435bc9d05
sigristarisa <arisasigrist.ch@gmail.com>
2022-05-03 09:57:36 +0200

    created calculateWater()

--- src/index.js
+++ src/index.js
@@ -1,13 +1,21 @@
 const starterForm = document.querySelector("#starter-form");
 const flourQuantity = document.querySelector("#flour-quantity");
+const waterQuantity = document.querySelector("#water-quantity");
 
 const calculateFlour = (starterValue) => {
   return (starterValue * 100) / 25;
 };
 
+const calculateWater = (flourValue) => {
+  return (flourValue * 62) / 100;
+};
+
 starterForm.addEventListener("submit", (e) => {
   e.preventDefault();
   const starterValue = e.target[0].value;
+  const flourValue = calculateFlour(starterValue);
+  const waterValue = calculateWater(flourValue);
 
-  flourQuantity.innerText = calculateFlour(starterValue);
+  flourQuantity.innerText = flourValue;
+  waterQuantity.innerText = waterValue;
 });

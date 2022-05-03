668da5ec67cf97356e1a946686a9955f7e163f1c
sigristarisa <arisasigrist.ch@gmail.com>
2022-05-03 09:49:58 +0200

    created calculateFlour()

--- src/index.js
+++ src/index.js
@@ -1,5 +1,13 @@
 const starterForm = document.querySelector("#starter-form");
+const flourQuantity = document.querySelector("#flour-quantity");
+
+const calculateFlour = (starterValue) => {
+  return (starterValue * 100) / 25;
+};
 
 starterForm.addEventListener("submit", (e) => {
   e.preventDefault();
+  const starterValue = e.target[0].value;
+
+  flourQuantity.innerText = calculateFlour(starterValue);
 });
